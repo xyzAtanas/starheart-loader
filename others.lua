3bc779bdc66db56b2d9d28bb0a0cc5a1be19782b
xyzAtanas <atanasdenkov93@gmail.com>
2026-02-07 18:03:10 +0200

    Fix other being tt

--- others.lua
+++ others.lua
@@ -1,6 +1,6 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-    Name = "Starheart - Twilight Terminal",
+    Name = "Starheart - Other",
     Icon = 105195470942547,
     LoadingTitle = "French fry loader",
     LoadingSubtitle = "by Atanas",
